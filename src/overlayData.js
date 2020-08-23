c6138ea304819fbe304f5e6b978ef7707aae8a58
Max Goodhart <c@chromakode.com>
2020-08-23 16:00:23 -0700

    Remove intl overlay from default set

--- src/overlayData.js
+++ src/overlayData.js
@@ -2,7 +2,6 @@ module.exports = {
   baseURL: 'https://stay.woke.net/overlays/',
   overlays: {
     'overlay.html': { kind: 'overlay' },
-    'overlay-intl.html': { kind: 'overlay' },
     'background.html': { kind: 'background' },
   },
 }
